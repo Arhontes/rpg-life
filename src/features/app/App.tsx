9c389e7b2f12a7aecf125bee2ac13d784f899782
Alex <arhontes@yandex.ru>
2023-11-19 08:54:14 +0300

    refactor: rename forders/file

--- src/features/app/App.tsx
+++ src/features/app/App.tsx
@@ -1,10 +1,10 @@
 import cn from 'classnames'
-import { CircleButton } from '../calculator'
+import { CentralNavigaton } from '../central-navigation'
 import s from './App.module.css'
 function App() {
     return (
         <div className={cn(s.app)}>
-            <CircleButton />
+            <CentralNavigaton />
         </div>
     )
 }
